f72316c4dd84f93119b1b6e255ed6af43f81c20d
AnkitBishen <ankitsinghanju@gmail.com>
2025-01-07 23:59:42 +0530

    missed to add cros pages

---
+++ internal/crosMid/crosMid.go
@@ -0,0 +1,14 @@
+package crosMid
+
+import "net/http"
+
+func CorsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		next.ServeHTTP(w, r)
+
+	})
+}
